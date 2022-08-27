d0536d0ba4d85bf735eeddf1d72723ac9dc27d51
TIEN CHIH CHENG <aim840912@gmail.com>
2022-08-27 17:30:28 +0800

    pathfind complete

--- Assets/Scripts/Grid/GridDebugObject.cs
+++ Assets/Scripts/Grid/GridDebugObject.cs
@@ -5,15 +5,20 @@ using TMPro;
 
 public class GridDebugObject : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro textMeshPro;
-    private GridObject gridObject;
 
-    public void SetGridObject(GridObject gridObject)
+    [SerializeField] private TextMeshPro textMeshPro;
+
+
+    private object gridObject;
+
+    public virtual void SetGridObject(object gridObject)
     {
         this.gridObject = gridObject;
     }
 
-    private void Update(){
-        textMeshPro.text=gridObject.ToString();
+    protected virtual void Update()
+    {
+        textMeshPro.text = gridObject.ToString();
     }
+
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,17 @@ using UnityEngine;
 public class Testing : MonoBehaviour
 {
 
+    [SerializeField] private Unit unit;
+
     private void Start()
     {
     }
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+        }
     }
+
 }
